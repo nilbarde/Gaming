45f0f3272a1593b1f3fac5ec09e09a0597f69970
batman <34839591+nilbarde@users.noreply.github.com>
2018-09-26 00:16:40 +0530

    vibration and animation

--- chain_gui.py
+++ chain_gui.py
@@ -12,27 +12,60 @@ class chain_game():
 	def define_game_constants(self):
 		pygame.display.set_caption("My Game")
 
-		self.cell_width, self.cell_height = 40,40
-		self.edge_space, self.edge_width = 4,1
+		self.edge_space, self.edge_width, self.button_height = 4, 1, 0
+		self.grid_height, self.grid_width = 480,400
+		self.display_height, self.display_width = self.grid_height + 2*self.edge_space + self.button_height, self.grid_width + 2*self.edge_space
 		self.no_rows, self.no_cols = 12, 10
-		self.display_height, self.display_width = self.no_rows*self.cell_width + 2*self.edge_space, self.no_cols*self.cell_height + 2*self.edge_space
+		self.cell_width, self.cell_height = self.grid_width/self.no_cols, self.grid_height/self.no_rows
+		print(self.cell_height,self.cell_width)
 		self.gameDisplay = pygame.display.set_mode((self.display_width,self.display_height))
 		self.clock = pygame.time.Clock()
 
-		self.radius, self.vibration = [0,12,10,8], [[0,0,0,0],[0,3,0,0],[0,2,3,0],[0,1,2,3]]
+		self.root_radi = min(self.cell_width,self.cell_height)/3.33
+		print(self.root_radi)
+		self.radius, self.vibration = [0,int(self.root_radi),int(self.root_radi*0.84),int(self.root_radi*0.66),int(self.root_radi*0.5),int(self.root_radi)], [[0,0,0,0,0,0],[0,3,0,0,0,0],[0,2,3,0,0,0],[0,1,2,3,0,0],[0,1,2,3,4,0],[0,1,2,3,4,5]]
 		self.frequency = [[0,0,0,0],[0,100,0,0],[0,50,100,0],[0,25,50,100]]
-		self.speed = [[100000,100000,100000,100000],[100000,200,100000,100000],[100000,400,200,100000],[100000,800,400,200]]
+		self.speed = [[100000,100000,100000,100000,100000],[100000,200,100000,100000,100000],[100000,400,200,100000,100000],[100000,800,400,200,10000,10000]]
 		self.high_freq = 1000
 		self.high_speed = 100
+		self.move_steps = 40.0
 
 		self.max_balls = np.zeros((self.no_rows,self.no_cols),dtype=int)
-		self.ball_pos_root = np.zeros((self.no_rows,self.no_cols,4,3,2))
+		self.ball_pos_root = np.zeros((self.no_rows,self.no_cols,6,5,2))
 
 		self.mouse_pos = pygame.mouse.get_pos()
 		self.mouse_clk = pygame.mouse.get_pressed()
 
 		self.set_grid_vals()
 		self.define_colors()
+		
+		self.define_play_variables()
+		self.mode = "not moving"
+		pygame.display.update()
+
+	def move_circle(self,x_1,y_1,x_2,y_2,color):
+		x01 = np.array(x_1)
+		x02 = np.array(x_2)
+		y01 = np.array(y_1)
+		y02 = np.array(y_2)
+		x1,y1,x2,y2 = x_1,y_1,x_2,y_2
+		for i in range(len(x01)):
+			x1[i] = int(self.edge_space + self.cell_width*(0.5+x01[i]))
+			x2[i] = int(self.edge_space + self.cell_width*(0.5+x02[i]))
+			y1[i] = int(self.edge_space + self.cell_width*(0.5+y01[i]))
+			y2[i] = int(self.edge_space + self.cell_width*(0.5+y02[i]))
+		for i in range(int(self.move_steps)):
+			# self.draw_grid(color)
+			self.update_display()
+			for j in range(len(x1)):
+				x = int(x1[j] + ((i+1)*(x2[j]-x1[j])/self.move_steps))
+				y = int(y1[j] + ((i+1)*(y2[j]-y1[j])/self.move_steps))
+				pygame.draw.circle(self.gameDisplay, color , (x,y),self.radius[1])
+			pygame.display.update()
+			sleep(2.5/self.move_steps)
+		for i in range(len(x02)):
+			self.grid_balls[y02[i]][x02[i]] += 1
+			self.grid_player[y02[i]][x02[i]] = self.now_player
 
 	def set_grid_vals(self):
 		self.max_balls[0][0], self.max_balls[self.no_rows-1][0], self.max_balls[0][self.no_cols-1], self.max_balls[self.no_rows-1][self.no_cols-1] = 1, 1, 1, 1
@@ -56,24 +89,40 @@ class chain_game():
 				x, y = int(self.edge_space + self.cell_width*(0.5+j)), int(self.edge_space + self.cell_height*(0.25+i))
 				self.ball_pos_root[i][j][3][2][0], self.ball_pos_root[i][j][3][2][1] = x, y
 
+				x, y = int(self.edge_space + self.cell_width*(0.25+j)), int(self.edge_space + self.cell_height*(0.25+i))
+				self.ball_pos_root[i][j][4][0][0], self.ball_pos_root[i][j][4][0][1] = x, y
+				x, y = int(self.edge_space + self.cell_width*(0.25+j)), int(self.edge_space + self.cell_height*(0.75+i))
+				self.ball_pos_root[i][j][4][1][0], self.ball_pos_root[i][j][4][1][1] = x, y
+				x, y = int(self.edge_space + self.cell_width*(0.75+j)), int(self.edge_space + self.cell_height*(0.25+i))
+				self.ball_pos_root[i][j][4][2][0], self.ball_pos_root[i][j][4][2][1] = x, y
+				x, y = int(self.edge_space + self.cell_width*(0.75+j)), int(self.edge_space + self.cell_height*(0.75+i))
+				self.ball_pos_root[i][j][4][3][0], self.ball_pos_root[i][j][4][3][1] = x, y
+
+				x, y = int(self.edge_space + self.cell_width*(0.5+j)), int(self.edge_space + self.cell_height*(0.5+i))
+				self.ball_pos_root[i][j][5][0][0], self.ball_pos_root[i][j][5][0][1] = x, y
+				self.ball_pos_root[i][j][5][1][0], self.ball_pos_root[i][j][5][1][1] = x, y
+				self.ball_pos_root[i][j][5][2][0], self.ball_pos_root[i][j][5][2][1] = x, y
+				self.ball_pos_root[i][j][5][3][0], self.ball_pos_root[i][j][5][3][1] = x, y
+				self.ball_pos_root[i][j][5][4][0], self.ball_pos_root[i][j][5][4][1] = x, y
+
 	def define_colors(self):
 		self.black = (0,0,0)
 		self.white = (255,255,255)
-		self.red = (255,0,0)
-		self.green = (0,255,0)
-		self.blue = (0,0,255)
-		self.l_red = (170,0,0)
-		self.l_green = (0,170,0)
-		self.l_blue = (0,0,170)
+		self.dark_red = (255,0,0)
+		self.dark_green = (0,255,0)
+		self.dark_blue = (0,0,255)
+		self.red = (170,0,0)
+		self.green = (0,170,0)
+		self.blue = (0,0,170)
 
 	def define_play_variables(self):
 		self.player_color = {0:self.black, 1:self.red,2:self.blue,3:self.green}
 		self.player_names = {0:"self.black", 1:"red player",2:"blue player ",3:"green player "}
-		self.now_player, self.tot_players = 2, 3 
+		self.now_player, self.tot_players = 2, 2
 
 		self.grid_player = np.zeros((self.no_rows,self.no_cols),dtype=int)
 		self.grid_balls = np.zeros((self.no_rows,self.no_cols),dtype=int)
-		self.ball_pos = np.zeros((self.no_rows,self.no_cols,4,3,2))
+		self.ball_pos = np.zeros((self.no_rows,self.no_cols,6,5,2))
 		self.ball_pos = self.ball_pos_root.copy()
 
 		self.players_alive = np.ones(self.tot_players+1)
@@ -86,6 +135,10 @@ class chain_game():
 	def play(self):
 		self.define_play_variables()
 		self.game_loop()
+		self.mode = "moving"
+		self.update_display()
+		pygame.display.update()
+		self.mode = "not moving"
 
 	def game_loop(self):
 		self.update_display()
@@ -105,6 +158,12 @@ class chain_game():
 				self.update_display()
 				pygame.display.update()
 
+			self.my_clock += 1
+			if(self.my_clock%32001 == 0):
+				self.my_clock = 1
+			if(self.round_one==0):
+				self.game_over = not(self.game_active())
+
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					self.game_over = True
@@ -112,11 +171,6 @@ class chain_game():
 					if event.key == pygame.K_q:
 						self.game_over = True
 
-			self.my_clock += 1
-			if(self.my_clock%32001 == 0):
-				self.my_clock = 1
-			if(self.round_one==0):
-				self.game_over = not(self.game_active())
 
 	def update_display(self):
 		self.mouse_pos = pygame.mouse.get_pos()
@@ -132,7 +186,7 @@ class chain_game():
 
 	def game_active(self):
 		actives = 0
-		for i in range(self.tot_players):
+		for i in range(1,self.tot_players+1):
 			if(self.player_alive(i)):
 				actives += 1
 		if(actives>1):
@@ -149,17 +203,27 @@ class chain_game():
 
 	def draw_grid(self,color):
 		self.gameDisplay.fill(self.black)
-		for i in range(self.edge_space,self.display_width,self.cell_width):
-			pygame.draw.line(self.gameDisplay, color, (i,self.edge_space),(i,self.display_height-self.edge_space),self.edge_width)
-		for i in range(self.edge_space,self.display_height,self.cell_height):
-			pygame.draw.line(self.gameDisplay, color, (self.edge_space,i),(self.display_width-self.edge_space,i),self.edge_width)
+		for i in range(self.edge_space,(self.edge_space+self.grid_width+1),self.cell_width):
+			pygame.draw.line(self.gameDisplay, color, (i,self.edge_space),(i,(self.edge_space+(self.no_rows)*self.cell_height)),self.edge_width)
+		for i in range(self.edge_space,(self.edge_space+self.grid_height+1),self.cell_height):
+			pygame.draw.line(self.gameDisplay, color, (self.edge_space,i),((self.edge_space+(self.no_cols)*self.cell_width),i),self.edge_width)
+		self.mouse_pos = pygame.mouse.get_pos()
+		self.mouse_clk = pygame.mouse.get_pressed()
+		if(self.mode!="moving"):
+			if((self.display_width/2<self.mouse_pos[0]<self.display_width) and (self.display_height-self.button_height<self.mouse_pos[0]<self.display_height)):
+				if(self.mouse_clk[0]==1):
+					self.game_over = True
+					print("exit")
+					self.game_quit()
+		pygame.draw.rect(self.gameDisplay, self.dark_red, (self.display_width/2,self.display_height-self.button_height,self.display_width,self.display_height))
 
 	def draw_circle(self,row,col,color):
-		if(self.edge_space + self.cell_width*(col)<self.mouse_pos[0]<self.edge_space + self.cell_width*(1+col)):
-			if(self.mouse_clk[0]==1):
-				if(self.edge_space + self.cell_height*(row)<self.mouse_pos[1]<self.edge_space + self.cell_height*(1+row)):
-					self.cell_clicked(row,col)
-					print("clicked")
+		if(self.mode!="moving"):
+			if(self.edge_space + self.cell_width*(col)<self.mouse_pos[0]<self.edge_space + self.cell_width*(1+col)):
+				if(self.mouse_clk[0]==1):
+					if(self.edge_space + self.cell_height*(row)<self.mouse_pos[1]<self.edge_space + self.cell_height*(1+row)):
+						self.cell_clicked(row,col)
+						print("clicked")
 		for k in range(self.grid_balls[row][col]):
 			x, y = int(self.ball_pos[row][col][self.grid_balls[row][col]][k][0]), int(self.ball_pos[row][col][self.grid_balls[row][col]][k][1])
 			pygame.draw.circle(self.gameDisplay, color , (x,y),self.radius[self.grid_balls[row][col]])
@@ -168,7 +232,9 @@ class chain_game():
 		if(self.now_player == self.grid_player[row][col] or 0 == self.grid_player[row][col]):
 			self.grid_balls[row][col] +=1
 			self.grid_player[row][col] = self.now_player
-			self.upgrade_grid(row,col)
+			self.mode = "moving"
+			self.upgrade_grid()
+			self.mode = "not moving"
 			x = self.now_player
 			x = ((x)%self.tot_players)+1
 			if(self.round_one==0):
@@ -186,73 +252,61 @@ class chain_game():
 				self.round_one -= 1
 			self.now_player = x
 
-	def upgrade_grid(self,row,col):
-		if(row==0 and col==0):
-			if(self.grid_balls[row][col]==2):
-				self.grid_balls[row][col] = 0 
-				self.grid_player[row][col] = 0
-				self.add_ball(row+1,col)
-				self.add_ball(row,col+1)
-		elif(row==0 and col==self.no_cols-1):
-			if(self.grid_balls[row][self.no_cols-1]==2):
-				self.grid_balls[row][self.no_cols-1] = 0 
-				self.grid_player[row][col] = 0
-				self.add_ball(row+1,self.no_cols-1)
-				self.add_ball(row,self.no_cols-2)
-		elif(row==self.no_rows-1 and col==0):
-			if(self.grid_balls[row][col]==2):
-				self.grid_balls[row][col] = 0 
-				self.grid_player[row][col] = 0
-				self.add_ball(row-1,0)
-				self.add_ball(row,1)
-		elif(row==self.no_rows-1 and col==self.no_cols-1):
-			if(self.grid_balls[row][col]==2):
-				self.grid_balls[row][col] = 0 
-				self.grid_player[row][col] = 0
-				self.add_ball(row-1,col)
-				self.add_ball(row,col-1)
-		elif(row==0):
-			if(self.grid_balls[row][col]==3):
-				self.grid_balls[row][col] = 0 
-				self.grid_player[row][col] = 0
-				self.add_ball(row,col+1)
-				self.add_ball(row+1,col)
-				self.add_ball(row,col-1)
-		elif(col==self.no_cols-1):
-			if(self.grid_balls[row][col]==3):
-				self.grid_balls[row][col] = 0 
-				self.grid_player[row][col] = 0
-				self.add_ball(row-1,col)
-				self.add_ball(row+1,col)
-				self.add_ball(row,col-1)
-		elif(row==self.no_rows-1):
-			if(self.grid_balls[row][col]==3):
-				self.grid_balls[row][col] = 0 
-				self.grid_player[row][col] = 0
-				self.add_ball(row-1,col)
-				self.add_ball(row,col+1)
-				self.add_ball(row,col-1)
-		elif(col==0):
-			if(self.grid_balls[row][col]==3):
-				self.grid_balls[row][col] = 0 
-				self.grid_player[row][col] = 0
-				self.add_ball(row-1,col)
-				self.add_ball(row,col+1)
-				self.add_ball(row+1,col)
+	def upgrade_grid(self):
+		x1,y1,x2,y2 = [],[],[],[]
+		for i in range(self.no_rows):
+			for j in range(self.no_cols):
+				if(self.grid_balls[i][j]>=self.max_balls[i][j]+1):
+					# self.grid_balls[i][j] = 0
+					# self.grid_player[i][j] = 0
+					if(j>0):
+						self.grid_balls[i][j] -= 1
+						y1.append(i)
+						x1.append(j)
+						y2.append(i)
+						x2.append(j-1)
+					if(i<self.no_cols-1):
+						self.grid_balls[i][j] -= 1
+						y1.append(i)
+						x1.append(j)
+						y2.append(i+1)
+						x2.append(j)
+					if(j<self.no_cols-1):
+						self.grid_balls[i][j] -= 1
+						y1.append(i)
+						x1.append(j)
+						y2.append(i)
+						x2.append(j+1)
+					if(i>0):
+						self.grid_balls[i][j] -= 1
+						y1.append(i)
+						x1.append(j)
+						y2.append(i-1)
+						x2.append(j)
+					if(self.grid_balls[i][j]>0):
+						self.grid_balls[i][j] -= 1
+						y1.append(i)
+						x1.append(j)
+						y2.append(i)
+						x2.append(j)
+					else:
+						self.grid_player[i][j] = 0
+		if(len(x1)>0):
+			self.move_circle(x1,y1,x2,y2,self.player_color[self.now_player])
+			self.upgrade_grid()
 		else:
-			if(self.grid_balls[row][col]==4):
-				self.grid_balls[row][col] = 0 
-				self.grid_player[row][col] = 0
-				self.add_ball(row-1,col)
-				self.add_ball(row,col+1)
-				self.add_ball(row+1,col)
-				self.add_ball(row,col-1)
+			return
 
 	def add_ball(self,row,col):
 		self.grid_balls[row][col] += 1
 		self.grid_player[row][col] = self.now_player
 		self.upgrade_grid(row,col)
 
+	def game_quit(self):
+		pygame.quit()
+		quit()
+
 game = chain_game()
 game.play()
-
+print("sleeping")
+sleep(4)
